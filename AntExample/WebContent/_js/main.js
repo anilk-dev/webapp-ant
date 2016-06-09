1d7e15f90c04405a7c07c075f838ff62db05f74e
Anil Kumar <anil@datakare.org>
2016-06-10 02:40:15 +0530

    Added sample ant web project code

---
+++ AntExample/WebContent/_js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.slider1').bxSlider();
+  $('.slider2').bxSlider({
+	slideWidth: 300,
+    minSlides: 2,
+    maxSlides: 3,
+    slideMargin: 10
+  });
+});
---
+++ AntExample/dockerfile/Dockerfile
@@ -0,0 +1,4 @@
+FROM tomcat:7.0
+MAINTAINER "anil.k@irlabs.io"
+ADD AntExample.war /usr/local/tomcat/webapps/
+
